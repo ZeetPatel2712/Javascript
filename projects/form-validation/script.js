73ba3137ce430f75239ebf27256095fcac80b5cc
ZeetGeek <zeetgeek435@gmail.com>
2024-05-22 11:29:00 +0530

    working on the form validation.

--- projects/form-validation/script.js
+++ projects/form-validation/script.js
@@ -1,98 +1,34 @@
 "use strict";
 const signUpForm = document.querySelector(".sign_up_form");
-const emailField = document.querySelector(".email_field");
-const inputField = document.querySelectorAll(".input_field");
-const passEye = document.querySelectorAll(".pass_eye");
+const passEyes = document.querySelectorAll(".pass_eye");
 
-// eye open and close logic
-passEye.forEach((item) => {
-      item.addEventListener("click", () => {
-            const passInput = item.previousElementSibling;
-
-            if (passInput.getAttribute("type") === "text") {
-                  passInput.setAttribute("type", "password");
-                  item.classList.replace("iconoir-eye-closed", "iconoir-eye");
-            } else {
-                  passInput.setAttribute("type", "text");
-                  item.classList.replace("iconoir-eye", "iconoir-eye-closed");
-            }
-      });
+// Toggle password visibility
+passEyes.forEach((eye) => {
+     eye.addEventListener("click", () => {
+          const passInput = eye.previousElementSibling;
+          if (passInput.getAttribute("type") === "text") {
+               passInput.setAttribute("type", "password");
+               eye.classList.replace("iconoir-eye-closed", "iconoir-eye");
+          } else {
+               passInput.setAttribute("type", "text");
+               eye.classList.replace("iconoir-eye", "iconoir-eye-closed");
+          }
+     });
 });
 
-// click on sign up button
+// Form submission handling
 signUpForm.addEventListener("submit", (e) => {
-      e.preventDefault();
+     e.preventDefault();
 
-      let emptyFieldValidation = false;
-
-      inputField.forEach((item) => {
-            const inputElement = item.querySelector("input");
-            const errorElement = item.nextElementSibling;
-
-            inputElement.value.trim().length <= 0
-                  ? errorElement.classList.remove("hide")
-                  : (emptyFieldValidation = true);
-      });
-
-      console.log(formValidation());
-
-      //  if (formValidation() && emptyFieldValidation) {
-      //        console.log("form submitted 🚀.");
-      //  } else {
-      //        console.log("something wrong 😔.");
-      //  }
+     validateForm() ? console.log("form submitted 🚀") : console.log("something wrong 😔");
 });
 
-// form validation logic
-const formValidation = () => {
-      let validationAns = false;
 
-      inputField.forEach((item) => {
-            const inputElement = item.querySelector("input").value.trim();
-            const elementName = item.querySelector("input").getAttribute("name");
-            const errorElement = item.nextElementSibling;
-
-            if (elementName === "email") {
-                  const emailPattern =
-                        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-                  if (!inputElement.match(emailPattern)) {
-                        errorElement.classList.remove("hide");
-                  } else {
-                        errorElement.classList.add("hide");
-                  }
-            }
-
-            if (elementName === "password") {
-                  const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-
-                  if (!inputElement.match(passwordPattern)) {
-                        errorElement.classList.remove("hide");
-                  } else {
-                        errorElement.classList.add("hide");
-                  }
-            }
-
-            if (elementName === "confirm_password") {
-                  const passwordValue = document.querySelector(".password_field input").value.trim();
-
-                  if (inputElement.length === 0) {
-                        errorElement.classList.remove("hide");
-                  } else if (passwordValue !== inputElement) {
-                        errorElement.classList.remove("hide");
-                  } else {
-                        errorElement.classList.add("hide");
-                  }
-            }
-      });
-
-      return validationAns;
-};
-
-// form validation when write on input
-inputField.forEach((item) => {
-      const inputElement = item.querySelector("input");
-      inputElement.addEventListener("keyup", () => {
-            formValidation();
-      });
-});
+const successMsg = () => {
+     return `
+         <div class="success">
+                                   <i class="iconoir-double-check"></i>
+                                   <span>Success Message</span>
+                              </div>
+     `;
+}
