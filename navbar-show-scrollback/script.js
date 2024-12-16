a800997de6868d52a232872f868997d050faf777
ZeetPatel2712 <zeetpatel2712@gmail.com>
2024-12-16 21:23:17 +0530

    create a new javascript projects.

---
+++ navbar-show-scrollback/script.js
@@ -0,0 +1,13 @@
+// let lastScrollY = window.scrollY;
+
+// window.addEventListener("scroll", () => {
+//     const header = document.querySelector(".header");
+
+//     if (window.scrollY > lastScrollY) {
+//         header.classList.add("hidden");
+//     } else {
+//         header.classList.remove("hidden");
+//     }
+
+//     lastScrollY = window.scrollY;
+// });
